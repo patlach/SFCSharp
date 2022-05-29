b31c201d0e4b149ce909db84b624235f697dec9f
Egor Polushin <EPolushin@it-one.ru>
2022-05-29 20:03:21 +0300

    Add module 5.3, 5.5

---
+++ SFCSharp/Module_5.3/GetArrayFromConsoleMethod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_5._3
+{
+    internal class GetArrayFromConsoleMethod
+    {
+        public int[] GetArrayFromConsole(ref int num)
+        {
+            var result = new int[num];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+
+            for (int i = 0; i < result.Length; i++)
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            return result;
+        }
+    }
+}
--- SFCSharp/Module_5.3/Program.cs
+++ SFCSharp/Module_5.3/Program.cs
@@ -6,19 +6,23 @@ namespace Module_5._3
     {
         static void Main(string[] args)
         {
-            var someName = "Егор";
+            var someName = "Egor";
+
             Console.WriteLine(someName);
 
-            GetName(someName);
+            GetName(out someName, out string oldname);
             Console.WriteLine(someName);
+            Console.WriteLine(oldname);
 
             Console.ReadKey();
         }
-
-        static void GetName(string name)
+        //int SumNumbers(ref int num1, in int num2, out int num3, int num4) { };
+        static void GetName(out string name, out string oldname)
         {
+            oldname = "Егор";
             Console.WriteLine("Enter name");
             name = Console.ReadLine();
         }
+
     }
 }
