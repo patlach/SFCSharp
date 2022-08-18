ea65590015e075fbbea1ecde1ad42acd0e791043
Egor Polushin <EPolushin@it-one.ru>
2022-08-18 11:52:43 +0300

    Add modules 14 and 15

---
+++ SFCSharp/Module_14.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Module_14._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            var a = people.Where(p => p.StartsWith("А")).OrderBy(p => p).ToList();
+
+            foreach (string s in a)
+                Console.WriteLine(s);
+        }
+    }
+}
